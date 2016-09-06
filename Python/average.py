fa75c8a7c7d9b2fa43c00de7e225844e11ed8d29
Anish Gheewala <agheewala1231@gmail.com>
2016-09-06 09:13:34 -0500

    pushing python

---
+++ Python/average.py
@@ -0,0 +1,2 @@
+myArray = [1, 2, 5, 10, 255, 3]
+sum(myArray) / float(len(myArray))
---
+++ Python/multipy.py
@@ -0,0 +1,5 @@
+array = [2,4,10,16]
+newArray = []
+for item in array:
+    newArray.append(item*5)
+    print (newArray)
